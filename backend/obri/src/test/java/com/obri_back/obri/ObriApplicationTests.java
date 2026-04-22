459b18a5f63d8a89d8fde3eb8fa0f1bc20c17f23
dlawotjr1999 <dlawotjr1999@naver.com>
2026-04-22 13:46:52 +0900

    극극극 초기 세팅

---
+++ backend/obri/src/test/java/com/obri_back/obri/ObriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obri_back.obri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
